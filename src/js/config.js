567038abad3014d3a599bbfae5fde920aa70c221
nsferattu <bb1sixx@gmail.com>
2026-03-11 19:39:37 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '144d9f67-ad40-40b3-84c9-8640879f6f91';
+export const MODAL_CLOSE_SEC = 2.5;
