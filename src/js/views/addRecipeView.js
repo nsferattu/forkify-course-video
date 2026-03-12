581580bce2029b5f0aae1585ef313002a62606cb
nsferattu <bb1sixx@gmail.com>
2026-03-12 18:51:14 +0100

    Implemented saving user-inputted recipe directions to localStorage. Replaced 'toggleWindow' with explicit 'closeWindow' method to prevent unexpected reopening.

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -9,11 +9,18 @@ class AddRecipeView extends View {
   _overlay = document.querySelector('.overlay');
   _btnOpen = document.querySelector('.nav__btn--add-recipe');
   _btnClose = document.querySelector('.btn--close-modal');
+  _originalMarkup = '';
 
   constructor() {
     super();
+    this._originalMarkup = this._parentElement.innerHTML;
+
     this._addHandlerShowWindow();
     this._addHandlerHideWindow();
+  }
+
+  restoreMarkup() {
+    this._parentElement.innerHTML = this._originalMarkup;
   }
 
   toggleWindow() {
@@ -21,13 +28,22 @@ class AddRecipeView extends View {
     this._window.classList.toggle('hidden');
   }
 
+  closeWindow() {
+    this._overlay.classList.add('hidden');
+    this._window.classList.add('hidden');
+
+    setTimeout(() => {
+      this.restoreMarkup();
+    }, 500);
+  }
+
   _addHandlerShowWindow() {
     this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
   }
 
   _addHandlerHideWindow() {
-    this._btnClose.addEventListener('click', this.toggleWindow.bind(this));
-    this._overlay.addEventListener('click', this.toggleWindow.bind(this));
+    this._btnClose.addEventListener('click', this.closeWindow.bind(this));
+    this._overlay.addEventListener('click', this.closeWindow.bind(this));
   }
 
   addHandlerUpload(handler) {
